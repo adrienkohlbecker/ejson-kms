b3ad190faa078996f9756802750c8ed76d570e8f
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2016-09-20 10:07:55 +0200

    Refactor common code

--- formatter/json_test.go
+++ formatter/json_test.go
@@ -1,39 +1,7 @@
 package formatter
 
-import (
-	"bytes"
-	"io/ioutil"
-	"testing"
-
-	"github.com/adrienkohlbecker/ejson-kms/model"
-	"github.com/stretchr/testify/assert"
-)
+import "testing"
 
 func TestJSON(t *testing.T) {
-
-	var b bytes.Buffer
-	items := make(chan Item, 2)
-	items <- Item{
-		Credential: model.Credential{
-			Name: "my_credential",
-		},
-		Plaintext: "my value",
-	}
-	items <- Item{
-		Credential: model.Credential{
-			Name: "another_one",
-		},
-		Plaintext: "string with \"quotes\"",
-	}
-	close(items)
-
-	err := JSON(&b, items)
-	assert.NoError(t, err)
-	exported, goErr := ioutil.ReadAll(&b)
-	assert.NoError(t, goErr)
-
-	expected, goErr := ioutil.ReadFile("./testdata/json.json")
-	assert.NoError(t, goErr)
-	assert.Equal(t, string(expected), string(exported))
-
+	testFormatter(t, JSON, "./testdata/json.json")
 }
